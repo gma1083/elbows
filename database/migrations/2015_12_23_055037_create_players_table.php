da72f6e8e846f30ce2015e0f1f03b3ef0cc0dd21
Gregory Arnheiter <garnheit@ucsc.edu>
2015-12-30 15:15:15 -0800

    First commit  - App Set-Up

---
+++ database/migrations/2015_12_23_055037_create_players_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePlayersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('players', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamps();
+            $table->string('username')->unique();
+            $table->string('password');
+            $table->integer('privilege');
+            $table->string('rank');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('players');
+    }
+}
